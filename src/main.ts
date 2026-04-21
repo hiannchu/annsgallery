404c3317ada18c5ce7fce1b9c1ff31d3c5840163
Ann <ann.chu.creator@gmail.com>
2026-04-21 22:35:29 +0800

    update to apply vercel analytics

--- src/main.ts
+++ src/main.ts
@@ -1,6 +1,8 @@
 import { createApp } from 'vue'
 import App from './App.vue'
+import { inject } from '@vercel/analytics'
 
 window.addEventListener('contextmenu', e => e.preventDefault())
 
+inject()
 createApp(App).mount('#app')
